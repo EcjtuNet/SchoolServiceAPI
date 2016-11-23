65f8cc30eef27541d7c3e50d53f0c36e824181a2
superkevingit <superkevingit@gmail.com>
2016-11-23 16:30:33 +0800

    登录类

--- app.py
+++ app.py
@@ -11,50 +11,58 @@ payload = config.get('payload')
 username  = config.get('student_id')
 password = config.get('cas_password')
 
-def get_lt_value(headers, login_url):
-    cas_page = requests.get(login_url, headers=headers)
-    soup = BeautifulSoup(cas_page.text, "lxml")
-    lt_value = soup.find("input", {"name": "lt"}).get('value')
-    return lt_value
+class login:
+    def __init__(self):
+        return
 
-def get_ticket_url(headers, login_url, payload, encodedService, service, username, password, lt):
-    payload["encodedService"] = encodedService
-    payload['service'] = service
-    payload['username'] = username
-    payload['password'] = password
-    payload['lt'] = lt
+    @classmethod
+    def get_lt_value(this, headers, login_url):
+        cas_page = requests.get(login_url, headers=headers)
+        soup = BeautifulSoup(cas_page.text, "lxml")
+        lt_value = soup.find("input", {"name": "lt"}).get('value')
+        return lt_value
 
-    ticket_page = requests.post(login_url, data=payload, headers=headers).text
-    pattern = re.findall(ur'\s\swindow\.location\.href="(.*)"\+cookie', ticket_page)
-    ticket_url = pattern[0]
-    return ticket_url
+    @classmethod
+    def get_ticket_url(this, headers, login_url, payload, encodedService, service, username, password, lt):
+        payload["encodedService"] = encodedService
+        payload['service'] = service
+        payload['username'] = username
+        payload['password'] = password
+        payload['lt'] = lt
 
-def get_login_cookie(ticket_url, headers):
-    page = requests.get(ticket_url, headers=headers, allow_redirects=False)
-    return page.cookies
+        ticket_page = requests.post(login_url, data=payload, headers=headers).text
+        pattern = re.findall(ur'\s\swindow\.location\.href="(.*)"\+cookie', ticket_page)
+        ticket_url = pattern[0]
+        return ticket_url
 
-def get_login(cookies , headers, info_url):
-    info_page = requests.get(info_url, cookies=cookies, headers=headers)
-    return info_page.text
+    @classmethod
+    def get_login_cookie(this, ticket_url, headers):
+        page = requests.get(ticket_url, headers=headers, allow_redirects=False)
+        return page.cookies
+
+    @classmethod
+    def get_login(this, cookies , headers, info_url):
+        info_page = requests.get(info_url, cookies=cookies, headers=headers)
+        return info_page.text
 
 # portal
-lt = get_lt_value(headers, login_url)
+lt = login.get_lt_value(headers, login_url)
 encodeService = "http%3a%2f%2fportal.ecjtu.edu.cn%2fdcp%2findex.jsp"
 service = "http://portal.ecjtu.edu.cn/dcp/index.jsp"
 info_url = "http://portal.ecjtu.edu.cn/dcp/getPortalData?sPage=home&gId=null&user_id=null&cid=null&template_type=1"
 
-ticket_url = get_ticket_url(headers, login_url, payload, encodeService, service, username, password, lt)
-login_cookie = get_login_cookie(ticket_url, headers)
-info_page = get_login(login_cookie, headers, info_url)
+ticket_url = login.get_ticket_url(headers, login_url, payload, encodeService, service, username, password, lt)
+login_cookie = login.get_login_cookie(ticket_url, headers)
+info_page = login.get_login(login_cookie, headers, info_url)
 print info_page
 
 # 教务系统
-lt = get_lt_value(headers, login_url)
+lt = login.get_lt_value(headers, login_url)
 encodeService = "http%3a%2f%2fjwxt.ecjtu.jx.cn%2fstuMag%2fLogin_dcpLogin.action"
 service = "http://jwxt.ecjtu.jx.cn/stuMag/Login_dcpLogin.action"
 info_url = "http://jwxt.ecjtu.jx.cn/scoreQuery/stuScoreQue_getStuScore.action?item=0401"
 
-ticket_url = get_ticket_url(headers, login_url, payload, encodeService, service, username, password, lt)
-login_cookie = get_login_cookie(ticket_url, headers)
-info_page = get_login(login_cookie, headers, info_url)
+ticket_url = login.get_ticket_url(headers, login_url, payload, encodeService, service, username, password, lt)
+login_cookie = login.get_login_cookie(ticket_url, headers)
+info_page = login.get_login(login_cookie, headers, info_url)
 print info_page
